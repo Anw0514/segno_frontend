519efa7034dc8962e4c5fc8ebd8c97e091c16445
anw0514 <anw0514@gmail.com>
2019-09-16 17:12:52 -0400

    added events

--- src/calendars/schedule.js
+++ src/calendars/schedule.js
@@ -4,7 +4,22 @@ import DayGridPlugin from '@fullcalendar/daygrid'
 
 function Schedule() {
     return (
-        <FullCalendar defaultView="dayGridMonth" plugins={[DayGridPlugin]} />
+        <FullCalendar 
+            defaultView="dayGridMonth" 
+            plugins={[DayGridPlugin]} 
+            events={[
+                {
+                    title: 'Rehearsal',
+                    start: '2019-09-12T10:30:00',
+                    end: '2019-09-12T11:30:00',
+                    extendedProps: {
+                        department: 'Group 1'
+                    },
+                    description: 'Scenes 1 and 2'
+                }
+            ]}
+            eventRender={info => console.log(info.event.extendedProps)}
+        />
     );
 }
 
